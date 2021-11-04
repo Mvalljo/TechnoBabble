b8472e25b56400371a15333970e2be1b3000e784
Maria Vallejo <maria.vallejo29@yahoo.com>
2021-11-03 23:09:33 -0500

    added comment

---
+++ public/js/comment.js
@@ -0,0 +1,22 @@
+const commentFormHandler = async function (event) {
+  event.preventDefault();
+
+  const postId = document.querySelector('input[name="post-id"]').value;
+  const body = document.querySelector('textarea[name="comment-body"]').value;
+
+  const response = await fetch('/api/comment', {
+    method: 'POST',
+    body: JSON.stringify({ postId, body }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.reload();
+  } else {
+    alert('Failed to create comment');
+  }
+};
+
+document
+  .querySelector('#new-comment-form')
+  .addEventListener('submit', commentFormHandler);
